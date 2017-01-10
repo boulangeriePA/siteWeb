ba5ec3510c7af5823f76853ce54b51a8850f093e
arichard.ir2018 <arichard.ir2018@C104-11.esaip.local>
2017-01-10 16:39:56 +0100

    changement menu

--- vues/templates/gauche.inc.php
+++ vues/templates/gauche.inc.php
@@ -10,30 +10,24 @@
         if (!is_null($this->lireDonnee('loginAuthentification'))) {
             echo "<h2>Utilisateur : <i>" . $this->lireDonnee('loginAuthentification') . "</i></h2>";
             //menu de gauche présent pour tous les utilisateurs
-            echo "<li><a href=\".?controleur=utilisateur&action=afficherCommandes\">-Afficher mes commandes</a></li>";
             echo "<li><a href=\".?controleur=utilisateur&action=coordonnees\">-Mes informations</a></li>";
+            echo "<li><a href=\".?controleur=utilisateur&action=afficherCommandes\">-Afficher mes commandes</a></li>";
             echo "<hr>";
-            echo "<li><a href=\".?controleur=utilisateur&action=commander\">-Passer une commande</a></li>";
             echo "<li><a href=\".?controleur=utilisateur&action=produits\">-Nos Produits</a></li>";
             echo "<li><a href=\".?controleur=utilisateur&action=formules\">-Nos Formules</a></li>";
+            echo "<li><a href=\".?controleur=utilisateur&action=commander\">-Passer une commande</a></li>";
         } else {
             echo "<li><a href=\".?controleur=connexion&action=seConnecter\">Se connecter</a></li>";
         }
         if (!is_null($this->lireDonnee('loginAuthentification')) && MaSession::get('role') == 1) {
             //ajout menu de gauche pour l'administrateur
             echo "<hr>";
-            //echo "<li><a href=\".?controleur=AdminPersonnes&action=creerPersonne\">-Cr&eacute;er une personne</a></li>";
-            echo "<li><a href=\".?controleur=AdminPersonnes&action=ajouterProduit\">-Ajouter un produit</a></li>";
-            //echo "<li><a href=\".?controleur=administrateur&action=creerClasse\">-Cr&eacute;er une classe</a></li>";
             //echo "<li><a href=\".?controleur=administrateur&action=creerUtilisateur&role=MaitreStage\">-Ajouter un ma&icirc;tre de stage</a></li>";
         }
-        if (!is_null($this->lireDonnee('loginAuthentification')) && ((MaSession::get('role') == 1 || MaSession::get('role') == 2))) {
+        if (!is_null($this->lireDonnee('loginAuthentification')) && ((MaSession::get('role') == 1 || MaSession::get('role') == 3))) {
             //ajout menu de gauche pour les boulangers
             echo "<hr>";
-            //echo "<li><a href=\".?controleur=utilisateur&action=creerEntreprise\">-Ajouter une entreprise</a></li>";
-            //echo "<li><a href=\".?controleur=utilisateur&action=MajEntreprise\">-M.A.J entreprise</a></li>";
-            //echo "<hr>";
-            //echo "<li><a href=\".?controleur=utilisateur&action=ajoutStage\">-Ajouter un stage</a></li>";
+            echo "<li><a href=\".?controleur=AdminPersonnes&action=ajouterProduit\">-Ajouter un produit</a></li>";
             echo "<li><a href=\".?controleur=utilisateur&action=commandesEnCours\">-Commandes en cours</a></li>";
         }
         ?>
